ec64be6c1c8f9f90c755a5ea90a906743cb5efd2
Alexis Olán Izquierdo <alexisolan05@icloud.com>
2023-03-31 14:40:40 -0600

    Fixing bug

--- src/helpers/loadSocialMedia.ts
+++ src/helpers/loadSocialMedia.ts
@@ -4,10 +4,13 @@ import type { url } from '@/types/common/common';
 export const fetchSocialMedia = async () => {
     const userToken = window.localStorage.getItem('user_token')?.replace(/['"]+/g, '')
     console.log(userToken);
-    
+
     const url: url = 'https://sandbox.core.api.brandme.la/api/v1/accounts/profile/networks'
     const resp = await fetch(url, {
         headers: {
+            "access-control-allow-origin": "*",
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            'Access-Control-Allow-Methods': '*',
             Authentication: `Bearer ${userToken}`,
         }
     })
